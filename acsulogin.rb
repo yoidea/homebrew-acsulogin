c1e4109408ba223986d7367ac99afcc5b6b3554d
yoidea <yoidea.cc@gmail.com>
2018-07-03 16:18:07 +0900

    Version up

--- acsulogin.rb
+++ acsulogin.rb
@@ -2,10 +2,10 @@ require "formula"
 
 class Acsulogin < Formula
   homepage "https://github.com/yoidea/acsulogin"
-  url "https://github.com/yoidea/acsulogin/archive/v0.1.tar.gz"
-  sha256 "d55e19a16599a8cd13c9b1f9b178d5081f5aa712723f953775e0c16b7b9d42e2"
+  url "https://github.com/yoidea/acsulogin/archive/v0.2.tar.gz"
+  sha256 "cfd0cc07c828e76a1470bdac3f79a67c9c7ddfd81a1aefbfbea79ab44d6e0901"
   head "https://github.com/yoidea/acsulogin.git"
-  version "0.1"
+  version "0.2"
 
   def install
     bin.install "acsu"
