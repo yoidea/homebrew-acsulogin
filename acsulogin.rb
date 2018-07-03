1f7805e16fa381ede6649a51613852c75139493b
yoidea <yoidea.cc@gmail.com>
2018-07-03 12:00:32 +0900

    Fix SHA256

--- acsulogin.rb
+++ acsulogin.rb
@@ -3,7 +3,7 @@ require "formula"
 class Acsulogin < Formula
   homepage "https://github.com/yoidea/acsulogin"
   url "https://github.com/yoidea/acsulogin/archive/v0.1.tar.gz"
-  sha256 "786618aa16451c7df18c3f97d3b79068f4db258f7f8b95be300cee651cc80076"
+  sha256 "d55e19a16599a8cd13c9b1f9b178d5081f5aa712723f953775e0c16b7b9d42e2"
   head "https://github.com/yoidea/acsulogin.git"
   version "0.1"
 
